7459aa44f6a373eb03a2b7d9bb49c80acbde4d0f
Austin Reeves <dogman_95@yahoo.com>
2019-08-13 16:59:00 -0700

    Basic Javascript Folder

---
+++ Basic Javascript Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+    var String = "This text is green!";
+    var result = String.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
