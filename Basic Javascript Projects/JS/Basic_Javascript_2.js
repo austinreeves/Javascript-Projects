41143a23f847c2f8b531a64ffb49c20e1ab19658
Austin Reeves <dogman_95@yahoo.com>
2019-08-13 17:27:20 -0700

    Basic Javascript 2
    adding my Basic HTML 2 and Basic Javascript 2 files

---
+++ Basic Javascript Projects/JS/Basic_Javascript_2.js
@@ -0,0 +1,11 @@
+function Lemons() {
+    var String = "This text is lemons!";
+    var result = String.fontcolor("purple");
+    document.getElementById("Purple_Text").innerHTML = result;
+}
+
+function ketchup() {
+    var on = "I have ketchup";
+    on += " on my napkin!";
+    document.getElementById("napkin").innerHTML = on;
+}
