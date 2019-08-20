eada515a1cb7f2f7155e7c5e1cd284f35e13bcd9
Austin Reeves <dogman_95@yahoo.com>
2019-08-20 11:35:34 -0700

    Basic Javascript 10
    adding my Basic Javascript 10 files

---
+++ Basic Javascript Projects/JS/Basic_Javascript_10.js
@@ -0,0 +1,52 @@
+function Call_Loop() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+function lengthFunction() {
+    var string = "How are you?";
+    var result = string.length;
+    document.getElementById("stringLength").innerHTML = result;
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+    Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function array_Function() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Array").innerHTML = "In this picture, the cat is " +
+        Cat_Picture[2] + ".";
+}
+
+function constant_function() {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " +
+    Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
+
+    var X = 50;
+    document.write(X);
+    {
+        let X = 25;
+        document.write("br" + X);
+    }
+    document.write("br" + X);
+
